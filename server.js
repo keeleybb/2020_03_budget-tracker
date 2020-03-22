6669dcb6507733c990952a8f03d1c87f1bf9af7c
keeleybb <keeleybb@users.noreply.github.com>
2020-03-22 12:23:08 -0600

    fn

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgets";
 
 //Connecting/Creating my Db
 mongoose.connect(MONGODB_URI);
