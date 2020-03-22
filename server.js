091ea2f642519d718713dffd3851363597f2df40
keeleybb <keeleybb@users.noreply.github.com>
2020-03-22 10:40:46 -0600

    Heroku updates

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+//Connecting/Creating my Db
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
